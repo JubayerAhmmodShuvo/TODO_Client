535f879f09e8e338e865283256841ee917f40bc9
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2022-05-18 19:42:52 +0600

    firebase env done

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0uhaMbmTxar9uM7Ooa91SKGiFq1mN4YU",
-  authDomain: "todo-app-c8f85.firebaseapp.com",
-  projectId: "todo-app-c8f85",
-  storageBucket: "todo-app-c8f85.appspot.com",
-  messagingSenderId: "323724044530",
-  appId: "1:323724044530:web:cf6ab4cd6f96a440e5c86b",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+
 };
 
 // Initialize Firebase
